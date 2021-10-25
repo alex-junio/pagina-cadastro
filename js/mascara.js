55156825f758ba904cd84ac0110babfecece3799
alex <aleks.junio@gmail.com>
2021-10-24 21:05:55 -0300

    ajustes finais

--- js/mascara.js
+++ js/mascara.js
@@ -1,7 +1,7 @@
 //adicionando máscara em alguns campos
 
-$(function(){ $("#cpf").mask("000.000.000-00");});
-$(function(){ $("#rg").mask("0000000000");});
-$(function(){ $("#cep").mask("00000-000");});
-$(function(){ $("#telefone").mask("(00) 0000-0000");});
-$(function(){ $("#celular").mask("(00) 00000-0000");});
+$(function() { $("#cpf").mask("000.000.000-00"); });
+$(function() { $("#rg").mask("0000000000"); });
+$(function() { $("#cep").mask("00000-000"); });
+$(function() { $("#telefone").mask("(00) 0000-0000"); });
+$(function() { $("#celular").mask("(00) 00000-0000"); });
